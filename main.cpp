6a9bd140157bb7ae13850547cb4ebf2f893f7050
Edgar Magdaleno <edgarmv97@gmail.com>
2016-12-08 18:02:25 -0800

    Added comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include "Boggle.h"
 
+/*
+	Boggle constructor loads the dictionary, creates a random board and solves it.
+	Function newGame creates a random board and solves it.
+	The solution is always stored at boggle->words.
+
+	An example of how to print the board solution is shown below.
+*/
+
 int main() {
 	Boggle *boggle = new Boggle();
 	boggle->newGame();
